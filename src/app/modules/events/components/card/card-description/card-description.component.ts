0bbd6efbfb4e1487c752b587984cff600ba2dcd2
Jose Daniel Oropeza Soria <danioropezasoria@gmail.com>
2022-12-07 11:45:57 -0400

    Add example of I18NEXT_NAMESPACE provider in Card component.

--- src/app/modules/events/components/card/card-description/card-description.component.ts
+++ src/app/modules/events/components/card/card-description/card-description.component.ts
@@ -3,15 +3,11 @@ import { I18NEXT_NAMESPACE, I18NextPipe } from "angular-i18next";
 
 @Component({
   selector: "card",
-  template: "<p>This is the Card component</p>",
+  template: "<p>This is the Card Description component</p>",
   providers: [{ provide: I18NEXT_NAMESPACE, useValue: "events/card" }, I18NextPipe]
 })
-export class CardComponent {
+export class CardDescriptionComponent {
   constructor(private i18nextPipe: I18NextPipe) {
     // i18nextPipe has the prefix "events/card". This works in html and typescript.
-    // With this provider it is possible to call the pipe in this way:
-    let textOne = i18nextPipe.transform("text-one");
-    // instead of:
-    textOne = i18nextPipe.transform("events/card:text-one");
   }
 }
